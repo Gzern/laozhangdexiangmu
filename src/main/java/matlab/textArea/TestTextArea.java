263f816fd05b2ee99d67aaaf99f49685cf603918
king <kimmartin@yeah.net>
2018-11-22 23:01:22 +0800

    fix circle dependency

--- src/main/java/matlab/textArea/TestTextArea.java
+++ src/main/java/matlab/textArea/TestTextArea.java
@@ -1,6 +1,5 @@
 package matlab.textArea;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,21 +8,19 @@ import javax.swing.*;
 @Configuration
 public class TestTextArea extends AbstractCustomTextArea {
 
-    @Autowired
-    private JScrollPane jScrollPane;
 
     @Override
     public void init() {
         this.setColumns(30);
         this.setRows(20);
         this.setLineWrap(false);
-        jScrollPane.setViewportView(this);
-        jScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        jScrollPane().setViewportView(this);
+        jScrollPane().setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
     }
 
     @Bean
     public JScrollPane jScrollPane() {
-        JScrollPane scrollPane = new JScrollPane();
-        return scrollPane;
+        JScrollPane jScrollPane = new JScrollPane();
+        return jScrollPane;
     }
 }
