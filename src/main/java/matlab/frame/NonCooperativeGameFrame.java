a876276fa61cd3101300e9f295c8c9a56a272465
king <kimmartin@yeah.net>
2018-12-04 23:50:38 +0800

    add params export

--- src/main/java/matlab/frame/NonCooperativeGameFrame.java
+++ src/main/java/matlab/frame/NonCooperativeGameFrame.java
@@ -3,12 +3,18 @@ package matlab.frame;
 import com.alibaba.fastjson.JSONObject;
 import matlab.project.NonCooperativeGame;
 import matlab.util.Constant;
+import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Component;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 @Component
 public class NonCooperativeGameFrame extends AbstractProjectFrame {
@@ -30,6 +36,7 @@ public class NonCooperativeGameFrame extends AbstractProjectFrame {
         JButton button1 = new JButton("演示程序一");
         JButton button2 = new JButton("演示程序二");
         JButton button3 = new JButton("演示程序三");
+        JButton button4 = new JButton("参数导出");
 
         button1.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -61,11 +68,36 @@ public class NonCooperativeGameFrame extends AbstractProjectFrame {
                 NonCooperativeGame.getInstance().spectrumPuProfit(n, paramsJSONObject);
             }
         });
+        button4.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (!checkParams()) {
+                    return;
+                }
+                String filePath = "C:\\Users\\king\\Desktop\\";
+                String fileName = "params.txt";
+                JSONObject paramsJSONObject = getParamsJSONObject();
+                Set<String> keySet = paramsJSONObject.keySet();
+                List<String> list = new ArrayList<String>();
+                for (String key : keySet) {
+                    list.add(new StringBuilder(key).append("=").append(paramsJSONObject.getString(key)).toString());
+                }
+                try {
+                    FileUtils.writeLines(new File(filePath + fileName), list);
+                    JOptionPane.showMessageDialog(null, "参数导出完毕!", ""
+                            , JOptionPane.PLAIN_MESSAGE);
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                    JOptionPane.showMessageDialog(null, "参数导出失败，请检查参数配置!", ""
+                            , JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
 
 
         contentPane.add(button1);
         contentPane.add(button2);
         contentPane.add(button3);
+        contentPane.add(button4);
 
 
         /**
@@ -79,6 +111,9 @@ public class NonCooperativeGameFrame extends AbstractProjectFrame {
 
         springLayout.putConstraint(SpringLayout.NORTH, button3, 150, SpringLayout.NORTH, contentPane);
         springLayout.putConstraint(SpringLayout.EAST, button3, -50, SpringLayout.EAST, contentPane);
+
+        springLayout.putConstraint(SpringLayout.NORTH, button4, 190, SpringLayout.NORTH, contentPane);
+        springLayout.putConstraint(SpringLayout.EAST, button4, -50, SpringLayout.EAST, contentPane);
 
     }
 
